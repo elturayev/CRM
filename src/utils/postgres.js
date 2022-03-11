cdb44c6aab4bce659353f1b3643ae817a4504d50
Dilshod Elturayev <dilshodelturayev1636@gmail.com>
2022-03-11 20:47:47 +0500

    elephand sql

--- src/utils/postgres.js
+++ src/utils/postgres.js
@@ -1,11 +1,7 @@
 import pg from 'pg'
 
 const pool = new pg.Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT
+  connectionString : 'postgres://tbruzflo:S1yGe9FfE_Dr2loSenOML9eKnYlrxG99@john.db.elephantsql.com/tbruzflo'
 })
 
 async function fetch(query,...params){
